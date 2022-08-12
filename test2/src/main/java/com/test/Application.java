0d2c64f65027556981115db02ecc8b11dec5b8f8
zhangzheng <568125122@qq.com>
2022-08-12 13:41:49 +0800

    test

--- test2/src/main/java/com/test/Application.java
+++ test2/src/main/java/com/test/Application.java
@@ -10,6 +10,7 @@ public class Application  {
 
     public static void main(String[] args) {
         System.out.println(111);
+        System.out.println(111);
         SpringApplication.run(Application.class, args);
     }
 }
